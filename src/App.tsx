a4bd7364ca5bff0e7079f2e94ace8012e9f74e45
higuchi-learn <haruki.higuchi.000@example.com>
2025-03-12 23:40:41 +0900

    add: 野菜のリストを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 //App.tsx
 
 import "./App.css";
+import VegetablesList from "./components/VegetablesList"
 
 function App() {
   return (
     <div>
       <h1>HelloWorld!</h1>
+      <VegetablesList />
     </div>
   );
 }
